da4d43141a7a77dbdad252cc3a0e18eee96b8a0c
cloud <48044103+jinclaudio@users.noreply.github.com>
2021-02-27 12:53:57 +0100

    Delete Progetto directory

--- Progetto/Locate.h
+++
@@ -1,9 +0,0 @@
-//
-// Created by jincl on 2021/2/27.
-//
-
-#ifndef PROGETTO_LOCATE_H
-#define PROGETTO_LOCATE_H
-
-#endif //PROGETTO_LOCATE_H
-void Locate(int x, int y);
--- Progetto/Map.h
+++
@@ -1,9 +0,0 @@
-//
-// Created by jincl on 2021/2/27.
-//
-
-#ifndef PROGETTO_MAP_H
-#define PROGETTO_MAP_H
-
-#endif //PROGETTO_MAP_H
-void print_boarder();
