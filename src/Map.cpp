a8e0d143f8d978e001207a3eba83e1ed5d25f5a9
cloud <48044103+jinclaudio@users.noreply.github.com>
2021-02-27 16:43:33 +0100

    Update Map.cpp

--- src/Map.cpp
+++ src/Map.cpp
@@ -14,14 +14,14 @@ const int LENGTH = 40;
 using namespace std;
 
 void print_boarder(){
-    for(int i = 0; i < WIDTH; i++){
-        Locate(BEGIN, i);
+    for(int i = 0; i < WIDTH; i++){ //finche non tocca l'altezza(18) 
+        Locate(BEGIN, i); //spostare il cursore in (0, i) perche begin = 0
         cout << "*";
     }
-    for(int i = 1; i < LENGTH; i++){
-        Locate(i, BEGIN);
+    for(int i = 1; i < LENGTH; i++){ //finche non tocca la lunghezza (40)
+        Locate(i, BEGIN); //spostare il cursore in (i, 0), ovver la riga sopra
         cout << "=";
-        Locate(i, WIDTH-1);
+        Locate(i, WIDTH-1); //spostare il cursore in (i, lunghezza-1), ovver la riga sotto, -1 perche altrimenti lascia uno spazio
         cout << "=";
     }
 }
